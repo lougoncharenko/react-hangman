fd710ef3102189c6fef45d782f50307c1d62a53c
Louisa Goncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-12-05 11:19:24 -0500

    Add files via upload

---
+++ src/assets/Container.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import words from './wordList.json'
+
+export const Container = () => {
+    const [wordGuess, setWordGuess] = useState('Test')
+
+  return (
+    <div>
+
+    </div>
+  )
+}
