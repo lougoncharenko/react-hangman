83905f44766036988c95c16ebee1626feb55e4b5
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-05 11:29:44 -0500

    return a rendom number from list

--- src/assets/Container.tsx
+++ src/assets/Container.tsx
@@ -2,10 +2,13 @@ import React, {useState} from 'react'
 import words from './wordList.json'
 
 export const Container = () => {
-    const [wordGuess, setWordGuess] = useState('Test')
+    const [wordGuess, setWordGuess] = useState(() => {
+      return words[Math.floor(Math.random() * words.length)]
+    })
 
   return (
     <div>
+      <p>{wordGuess}</p>
 
     </div>
   )
