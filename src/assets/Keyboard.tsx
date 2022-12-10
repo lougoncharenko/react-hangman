7d96b578aa1c2c9e121ee9c6e0b45c2b4cacb415
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-10 01:08:22 -0500

    added keys

--- src/assets/Keyboard.tsx
+++ src/assets/Keyboard.tsx
@@ -1,4 +1,33 @@
 import React from 'react'
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
+
 
 const Keyboard = () => {
   return (
