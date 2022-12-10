82cb171c5f5b42a906cc7ca4c8c77d872cd74346
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-10 00:33:44 -0500

    created head element

--- src/assets/HangmanDrawing.tsx
+++ src/assets/HangmanDrawing.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import './HangmanDrawing.css'
 
+const Head = (
+  <div 
+    style = {{
+      width: '50px',
+      height: '50px',
+      borderRadius: '100%',
+      border: '10px, solid black',
+      position: 'absolute',
+      top: '50px',
+      right: '-30px'
+    }} />
+)
+
 export const HangmanDrawing = () => {
   return (
     <section className='hangman-drawing'>
