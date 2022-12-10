3d6a5579435a84bfb84a98ecea9db6a314fc1b94
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-10 01:06:55 -0500

    added logic to make letter appear if guessed correctly

--- src/assets/HiddenWord.tsx
+++ src/assets/HiddenWord.tsx
@@ -2,9 +2,24 @@ import React from 'react';
 import './HiddenWord.css'
 
 const HiddenWord = () => {
+    const word = 'test'
+    const guessedLetters = 't'
   return (
     <div className='hidden-word-container'> 
-      Text
+      {word.split('').map((letter, index) => (
+         <span className='hidden-letters' key={index}>
+             <span 
+             style = {{
+                 visibility: guessedLetters.includes(letter)
+                 ? "visible"
+                 : 'hidden'
+             }}
+             >
+                {letter}  
+             </span>
+            
+             </span> 
+  ))}
     </div>
   );
 }
