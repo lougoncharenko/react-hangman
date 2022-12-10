64f8154d532dd2c7aa07e3cfcfbc0569c60c3716
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-10 00:51:30 -0500

    changed filename

--- src/assets/Container.tsx
+++ src/assets/Container.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import words from './wordList.json'
 import './Container.css'
 import { HangmanDrawing } from './HangmanDrawing'
-import HangmanWord from './HangmanWord'
+import HiddenWord from './HiddenWord'
 import Keyboard from './Keyboard'
 
 export const Container = () => {
@@ -15,7 +15,7 @@ export const Container = () => {
     <div className= 'container' >
       <div className='results'> Lose Win</div>
       <HangmanDrawing />
-      <HangmanWord />
+      <HiddenWord />
       <Keyboard />
     </div>
   )
--- src/assets/HangmanWord.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HangmanWord = () => {
-  return (
-    <div>HangmanWord</div>
-  )
-}
-
-export default HangmanWord
