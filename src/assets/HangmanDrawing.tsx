c520025ca1ec2a4b99392ba7b575f8841438cc5d
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-10 00:43:12 -0500

    created body

--- src/assets/HangmanDrawing.tsx
+++ src/assets/HangmanDrawing.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import './HangmanDrawing.css'
 
-const Head = (
-  <div 
-    style = {{
-      width: '50px',
-      height: '50px',
-      borderRadius: '100%',
-      border: '10px, solid black',
-      position: 'absolute',
-      top: '50px',
-      right: '-30px'
-    }} />
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
 )
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+)
+
 
 export const HangmanDrawing = () => {
   return (
     <section className='hangman-drawing'>
         <div className='side' />
-        <div className='top'></div>
+        <div className='top' />
         <div className='middle'/>
         <div className='bottom' /> 
     </section>
